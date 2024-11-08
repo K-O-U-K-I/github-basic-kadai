b3f807b30e694ec0d29b166a8db9b5bac7ac0eba
K-O-U-K-I <huanglanshitian1@gmail.com>
2024-11-08 13:01:12 +0900

    kadai_014

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+function functionName(parameters) {
+  // 関数の処理
+  return value;  // 戻り値
+}
+
+const functionName = (parameters) => {
+  // 関数の処理
+  return value;  // 戻り値
+}
+
+const functionName = (parameters) => value;  // 戻り値
+
